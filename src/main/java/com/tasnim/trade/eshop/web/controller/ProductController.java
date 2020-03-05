617c99d14342c1fa3b896cacf2b62ac5ba02f0b4
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-03-05 16:56:37 +0330

    Product controller

---
+++ src/main/java/com/tasnim/trade/eshop/web/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.tasnim.trade.eshop.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/product")
+@Controller
+public class ProductController {
+
+    @GetMapping("/list")
+    public String index() {
+        return "/product/index";
+    }
+}
