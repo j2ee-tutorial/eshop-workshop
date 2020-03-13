d0d1249b06cb4b1e87027291e82ff5988b976232
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-03-13 23:50:48 +0330

    Application test

---
+++ src/test/java/com/tasnim/trade/eshop/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.tasnim.trade.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
