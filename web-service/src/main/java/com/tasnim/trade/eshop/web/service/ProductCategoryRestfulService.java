ec14fdc375db15ea05dca9f95a722367d827e1b3
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-06-04 15:45:26 +0430

    Product Categeory Rest Api

---
+++ web-service/src/main/java/com/tasnim/trade/eshop/web/service/ProductCategoryRestfulService.java
@@ -0,0 +1,31 @@
+package com.tasnim.trade.eshop.web.service;
+
+import com.tasnim.trade.eshop.api.ProductCategoryService;
+import com.tasnim.trade.eshop.dto.ProductCategory;
+import com.tasnim.trade.eshop.dto.Response;
+import com.tasnim.trade.eshop.dto.SuccessfulResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RequestMapping("/api/v1/productCategories")
+@RestController
+public class ProductCategoryRestfulService {
+
+    private final ProductCategoryService service;
+
+    public ProductCategoryRestfulService(ProductCategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Response> index() {
+        return new ResponseEntity<>(new SuccessfulResponse<>(service.findAll()), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Response> save(@RequestBody ProductCategory productCategory) {
+        return ResponseEntity.ok(new SuccessfulResponse<>(service.save(productCategory)));
+    }
+}
